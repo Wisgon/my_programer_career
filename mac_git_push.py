b901c5038d10da9f2f390b169cb0a5af579c8735
陈智龙 <286142377@qq.com>
2019-08-26 23:46:00 +0800

    pycharm setting

--- mac_git_push.py
+++ mac_git_push.py
@@ -3,5 +3,5 @@ import sys, os
 if len(sys.argv) == 1:
     raise TypeError("must at least one argument that is git commit message!")
 os.system("git add -f *")
-os.system("git commit -m '" + sys.argv[1] + "'")
+os.system("git commit -am '" + sys.argv[1] + "'")
 os.system("git push")
