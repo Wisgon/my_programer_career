aaa698de3b6dbf86be60c95ab40d9adee6469a9c
jhd <jhd@jhd.com>
2019-11-19 08:49:58 +0800

    gsd

--- mac_git_push.py
+++ mac_git_push.py
@@ -2,6 +2,7 @@ import sys, os
 
 if len(sys.argv) == 1:
     raise TypeError("must at least one argument that is git commit message!")
+os.system("git pull")
 os.system("git add -f *")
 os.system("git commit -am '" + sys.argv[1] + "'")
 os.system("git push")
