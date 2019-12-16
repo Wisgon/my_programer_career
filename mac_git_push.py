85cd9a38d834385b1ba18c4fad8e58bb6d5858ef
zhilong <oncwnuEYtYK86WiHKfUSMp97JMRA@git.weixin.qq.com>
2019-12-16 18:41:13 +0800

    'dfsf'

--- mac_git_push.py
+++ mac_git_push.py
@@ -4,5 +4,5 @@ if len(sys.argv) == 1:
     raise TypeError("must at least one argument that is git commit message!")
 os.system("git pull")
 os.system("git add -f *")
-os.system("git commit -am '" + sys.argv[1] + "'")
+os.system("git commit -m '" + sys.argv[1] + "'")
 os.system("git push")
